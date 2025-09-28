3f82193a7540b0ab43b516bf1bad2ab53ddeded9
rachitsharma300 <rachitsharma300@gmail.com>
2025-09-28 17:51:10 +0530

    init: Mock Test App Project file

---
+++ src/main/java/com/mocktest/app/MockTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.mocktest.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockTestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mocktest/app/MockTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mocktest.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
