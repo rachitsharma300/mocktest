124b45cc3f79d7d41bb1958bf74932c7fac97c93
rachitsharma300 <rachitsharma300@gmail.com>
2025-09-29 14:50:08 +0530

    feat: add PostCSS configuration and update Tailwind CSS config for content paths

--- mock-test-frontend/tailwind.config.js
+++ mock-test-frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",           // main HTML
+    "./src/**/*.{js,jsx,ts,tsx}"  // all JS/JSX/TS/TSX files in src
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
